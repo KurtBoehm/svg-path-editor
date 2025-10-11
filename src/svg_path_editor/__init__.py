1591c47fd82abe5fbce408a259ad36b61237cfc2
Kurt Böhm <kurbo96@gmail.com>
2025-10-11 17:04:15 +0200

    Add the MPL 2.0 license and the corresponding headers

--- src/svg_path_editor/__init__.py
+++ src/svg_path_editor/__init__.py
@@ -1,3 +1,9 @@
+# This file is part of https://github.com/KurtBoehm/svg_path_editor.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from .path_operations import optimize_path as optimize_path
 from .path_operations import reverse_path as reverse_path
 from .path_parser import PathParser as PathParser
